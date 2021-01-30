48b0bc56ef147ae6d993d9f6a5e5d353c76180fb
ryusuke <ryusuke_920@icloud.com>
2021-01-30 17:15:23 +0900

    素因数分解の追加

---
+++ 素因数分解/factorization.py
@@ -0,0 +1,18 @@
+def factorization(n):
+    arr = []
+    tmp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if tmp % i == 0:
+            cnt = 0
+            while tmp % i == 0:
+                cnt += 1
+                tmp //= i
+            arr.append([i, cnt])
+    if tmp != 1:
+        arr.append([tmp, 1])
+    if arr == []:
+        arr.append([n, 1])
+    return arr
+
+x = factorization(24)
+print(x)
