4744fc0eec1f8887b45853d4dda5b44ba457e102
ryusuke <ryusuke_920@icloud.com>
2021-01-30 17:32:52 +0900

    RLEの追加

---
+++ ランレングス圧縮/rle.py
@@ -0,0 +1,14 @@
+def rle(S):
+    tmp, cnt, ans = S[0], 1, ""
+    for i in range(1, len(S)):
+        if tmp == S[i]:
+            cnt += 1
+        else:
+            ans += tmp + str(cnt)
+            tmp = S[i]
+            count = 1
+    ans += tmp + str(cnt)
+    return ans
+
+s = "RRRLLRLRRLLLLRLRLRR"
+print(rle(s))
