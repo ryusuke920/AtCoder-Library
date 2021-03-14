0fb20623736208eedd397b62e2a5c52485a3692d
ryusuke <ryusuke_920@icloud.com>
2021-03-14 16:06:43 +0900

    add dijkstra.py

---
+++ Dijkstra/dijkstra.py
@@ -0,0 +1,20 @@
+from heapq import heappush, heappop
+import sys # 入力高速化
+input = sys.stdin.readline
+
+def dijkstra(s, graph): # (始点, グラフのリスト)
+    INF = 10 ** 18
+    dist = [INF] * n # INF で初期化
+    check = [False] * n　# Bool
+    dist[s] = 0
+    q = [(0, s)] # （距離・ノード）
+    while q:
+        v = heappop(q)[1] # 今いるノード
+        if check[v]: continue # すでに行っていたらcontinue
+        check[v] = True # 訪問済み
+        for i, j in graph[v]: # 先のノード・距離
+            if check[i] != False: continue
+            if dist[i] <= dist[v] + j: continue
+            dist[i] = dist[v] + j
+            heappush(q, (dist[i], i)) # 必ず[0]が距離になるように（優先度付きキュー）
+    return dist
