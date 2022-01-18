4241e5e0795688a771b0ca0fc0a8b8c70e239e7a
ryusuke <ryusuke_920@icloud.com>
2022-01-19 01:26:27 +0900

    Minimum Spanning Tree

--- MathLibrary/nCk_2.py
+++ MathLibrary/nCk_2.py
@@ -1,21 +1,30 @@
-# nCk.pyよりも高速かな？（powを外に出した）
 def combination(n, k):
     nCk = under = top = 1
-    mod = 10 ** 9 + 7
 
-    # 分母
     for i in range(2, k + 1):
         under *= i
-        under %= mod
 
-    # 分子
     for i in range(k):
         top *= (n - i)
-        top %= mod
 
-    nCk = top * pow(under, mod - 2, mod)
 
-    return nCk % mod
+    nCk = top // under
 
-h, w = map(int,input().split())
-print(combination(h + w - 2, h - 1))
+    return nCk 
+
+def factorization(n):
+    arr = []
+    tmp = n
+    for i in range(2, int(-(-n**0.5//1))+1):
+        if tmp % i == 0:
+            cnt = 0
+            while tmp % i == 0:
+                cnt += 1
+                tmp //= i
+            arr.append([i, cnt])
+    if tmp != 1:
+        arr.append([tmp, 1])
+    if arr == []:
+        arr.append([n, 1])
+    return arr
+print(combination(136,14) // 2 ** 6 // 9)
---
+++ MinumumSpanningTree/Kruskal.py
@@ -0,0 +1,39 @@
+# クラスカル法
+# 出典：https://atcoder.jp/contests/typical-algorithm/tasks/typical_algorithm_f
+
+class UnionFind:
+  def __init__(self, n):
+    self.n = n
+    self.p = [-1] * n
+
+  def leader(self, a):
+    while self.p[a] >= 0:
+      a = self.p[a]
+    return a
+
+  def merge(self, a, b):
+    x = self.leader(a)
+    y = self.leader(b)
+    if x == y: return x
+    if self.p[x] > self.p[y]:
+      x, y = y, x
+    self.p[x] += self.p[y]
+    self.p[y] = x
+    return x
+
+  def same(self, a, b): return self.leader(a) == self.leader(b)
+
+  def size(self, a): return -self.p[self.leader(a)]
+
+n, m = map(int, input().split())
+uf = UnionFind(n)
+# (a, b, cost)の無向グラフ
+g = [list(map(int, input().split())) for _ in range(m)]
+g.sort(key=lambda x: x[2])
+ans = 0
+for i, j, cost in g:
+    if uf.same(i, j): continue
+    uf.merge(i, j)
+    ans += cost
+
+print(ans)
