0c2f3316cd6e531295eca210f89f092859ade0f7
ryusuke920 <ryusuke920@icloud.com>
2022-02-06 18:19:10 +0900

    BFSの修正を行いました

--- BFS/BreadthFirstSearch.py
+++ BFS/BreadthFirstSearch.py
@@ -1,33 +1,37 @@
 from collections import deque
+
+def bfs(sy: int, sx: int) -> list:
+    '始点座標を受け取ってBFS'
+    INF = 10 ** 18
+    dist = [[INF] * w for _ in range(h)]
+    dist[sy][sx] = 0
+    
+    # 障害物の確認
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == '#':
+                dist[i][j] = -1
+    
+    # BFS
+    d = ((0, 1), (0, -1), (1, 0), (-1, 0))
+    q = deque()
+    q.append((sy, sx))
+
+    while q:
+        vy, vx = q.popleft()
+        for dy, dx in d:
+            y = vy + dy
+            x = vx + dx
+            if not (0 <= x < w and 0 <= y < h): continue
+            if grid[y][x] == '#': continue
+            if dist[y][x] != INF: continue
+            dist[y][x] = dist[vy][vx] + 1
+            q.append((y, x))
+    
+    return dist
+
 h, w = map(int,input().split())
-sy, sx = map(int,input().split())
-gy, gx = map(int,input().split())
-(sy, sx, gy, gx) = (sy - 1, sx - 1, gy - 1, gx - 1) # 0indexで設定する
-grid = [[] for _ in range(h)] # h * w のgridマスを用意する
+grid = [list(input()) for _ in range(h)]
 
-for i in range(h):
-    C = list(input())
-    for j in range(w):
-        grid[i] = C
-
-dist = [[10000] * w for _ in range(h)] # 各マスの最小距離を記録する２次元配列
-for i in range(h):
-    for j in range(w):
-        if grid[i][j] == "#": # gridマスが # になっている場合は通れないので便宜上 -1 とする
-            dist[i][j] = -1
-dist[sy][sx] = 0 # スタート地点は０距離で行ける
-
-q = deque()
-q.append([sy, sx]) # (スタートのy座標, スタートのx座標)
-d = [[1, 0], [-1, 0], [0, 1], [0, -1]] # ４方向への変化量
-
-while q:
-    v = q.popleft() # (今いるy座標, 今いるx座標)
-    for i, j in d: # 変化量を足していく（配列の長さである4回分）
-        if not (0 <= v[0] + i <= h - 1 and 0 <= v[1] + j <= w - 1): continue # 変化量を足した際に配列外になる場合は調べない
-        if grid[v[0] + i][v[1] + j] == "#": continue # マスが通れない場合は当然調べない
-        if dist[v[0] + i][v[1] + j] != 10000: continue # 10000でなければ既に調べたか行けない場所なので調べない
-        dist[v[0] + i][v[1] + j] = dist[v[0]][v[1]] + 1
-        q.append([v[0] + i, v[1] + j]) # 次に行く場所をdequeに追加する
-
-print(dist[gy][gx])
+dist = bfs(0, 0)
+print(*dist, sep='\n')
