40cae571bb13bc2e4a97b5012259fd2a0c1cd7ae
Yourein <rein.you.3.1@gmail.com>
2022-03-27 10:45:57 +0900

    Amend the Time complexity for FindCenter. It should be O(N) or O(NlogN)

--- Tree/FindCenter.py
+++ Tree/FindCenter.py
@@ -1,20 +1,25 @@
 # 木の中心を求める O(N^3)
-
+# 1-indexedで表された木の中心となりうる頂点の集合を改行区切りで出力します。
 from collections import deque
+from collections import defaultdict
 
-def bfs(s: int) -> list:
+def bfs(s: int) -> tuple:
     "幅優先探索を行い、頂点 s からの距離を求める"
     dist = [10000] * N
     dist[s] = 0
     q = deque([s])
+
+    vertexmemo = defaultdict(list)
+    vertexmemo[0].append(s)
     while q:
         v = q.popleft()
         for nxt in g[v]:
             if dist[nxt] == 10000:
                 dist[nxt] = dist[v] + 1
+                vertexmemo[dist[nxt]].append(nxt)
                 q.append(nxt)
     
-    return dist
+    return dist, vertexmemo
 
 N = int(input())
 
@@ -26,25 +31,18 @@ for _ in range(N - 1):
     g[a].append(b)
     g[b].append(a)
 
-dist = bfs(0)
-max_dist = max(dist)
-for i in range(N):
-    if dist[i] == max_dist:
-        # 木の直径を求める
-        diameter = max(bfs(i))
-        break
+dist, vmap = bfs(0)
+Dlist, LPM = bfs(vmap[max(dist)][0])
+Diameter = max(Dlist)
 
-# ある頂点が木の中心であるならば、すべての頂点への距離は「(木の直径 / 2)以下」となる
-center = []
-for i in range(N):
-    dist = bfs(i)
-    check = True
-    for j in range(N):
-        if dist[j] > (diameter + 1) // 2:
-            check = False
-    if check:
-        center.append(i + 1)
+FromA = bfs(LPM[0][0])[1]
+FromB = bfs(LPM[Diameter][0])[1]
+pre_center = FromA[Diameter//2] if Diameter%2 == 0 else FromA[Diameter//2]+FromA[(Diameter+1)//2]
+center = list()
 
-center.sort()
-for i in center:
+for i in FromB[Diameter//2] if Diameter%2 == 0 else FromB[Diameter//2]+FromB[(Diameter+1)//2]:
+    if i in pre_center:
+        center.append(i+1)
+
+for i in sorted(center):
     print(i)
