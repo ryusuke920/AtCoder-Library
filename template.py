3b3ac7ea15e2445a7faac8b3fc73da21969e1e9c
ryusuke920 <ryusuke920@icloud.com>
2022-05-07 23:17:09 +0900

    add template.py

---
+++ template.py
@@ -0,0 +1,38 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+printd = lambda *x : print(*x, file = sys.stderr)
+
+from math import ceil, floor, sin, cos, tan, acos, asin, atan, radians, factorial, exp, degrees
+from collections import defaultdict, deque, Counter
+from itertools import product, permutations, combinations, combinations_with_replacement
+from heapq import heapify, heappop, heappush
+from bisect import bisect, bisect_left, bisect_right
+
+def min(a: int, b: int) -> int:
+    "最小値"
+    return a if a <= b else b
+
+
+def max(a: int, b: int) -> int:
+    "最大値"
+    return a if a >= b else b
+
+def OutOfRange(h: int, w: int, vy: int, vx: int) -> bool:
+    "BFSなどの配列外参照"
+    d = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    for dy, dx in d:
+        y = vy + dy
+        x = vx + dx
+        if not (0 <= x < w and 0 <= y < h):
+            return False
+        else:
+            return True
+
+
+def main() -> None:
+    INF = 10 ** 18
+
+
+if __name__ == '__main__':
+    main()
