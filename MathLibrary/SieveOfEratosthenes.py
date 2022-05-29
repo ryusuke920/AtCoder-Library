e780eff84759ab03d6cea7f68c60853ffa05c384
ryusuke920 <ryusuke920@icloud.com>
2022-05-30 00:21:44 +0900

    modified エラトステネスの篩

--- MathLibrary/SieveOfEratosthenes.py
+++ MathLibrary/SieveOfEratosthenes.py
@@ -1,14 +1,17 @@
-def primes(n):
+def primes(n: int) -> list:
+    "素数の列挙を行う"
+
     is_prime = [True] * (n + 1)
     is_prime[0] = False
     is_prime[1] = False
-    for i in range(2, int(n**0.5) + 1):
-        if not is_prime[i]: continue
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if not is_prime[i]:
+            continue
         for j in range(i * 2, n + 1, i):
             is_prime[j] = False
+
     return [i for i in range(n + 1) if is_prime[i]]
 
-n = 100
-x = primes(n) # 素数の全列挙
-l = len(x) # 素数の数
-print(l)
+x = primes(100) # 素数の全列挙
+print(len(x), x)
