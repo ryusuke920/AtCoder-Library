13403e600639dd490887b51b26a4bb05aa407988
ryusuke920 <ryusuke920@icloud.com>
2022-07-15 02:31:44 +0900

    add test/

--- test/yosupo/Unionfind.test.py
+++ test/yosupo/Unionfind.test.py
@@ -1,11 +1,16 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
 
-from Tree.UnionFindTree import UnionFind
+'''
+import sys
+sys.path.append('../..')
+'''
+
+from Tree import UnionFindTree
 
 def main():
     n, q = map(int, input().split())
 
-    uf = UnionFind(n)
+    uf = UnionFindTree.UnionFind(n)
 
     for _ in range(q):
         t, u, v = map(int,input().split())
