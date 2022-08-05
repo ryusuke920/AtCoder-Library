c6f49d9e250fe0e18a0b39109a7068ed98f42466
ryusuke920 <ryusuke920@icloud.com>
2022-08-05 13:06:44 +0900

    modified .py

--- test/yosupo/DataStructure/Unionfind.test.py
+++ test/yosupo/DataStructure/Unionfind.test.py
@@ -1,23 +1,11 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
 
-import sys
-from pathlib import Path
-p = Path(__file__).parts
-sys.path.append('/'.join(p[:p.index('AtCoder-Library') + 1]))
-
-from Tree import UnionFindTree
+from Tree.UnionFindTree import UnionFind
 
 def main():
     n, q = map(int, input().split())
 
-    uf = UnionFindTree.UnionFind(n)
+    uf = UnionFind(n)
 
     for _ in range(q):
         t, u, v = map(int, input().split())
-        if t == 0:
-            uf.merge(u, v)
-        elif t == 1:
-            print(int(uf.same(u, v)))
-
-if __name__ == "__main__":
-    main()
