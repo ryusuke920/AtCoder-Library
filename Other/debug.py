862da358bb5abe356d8cdc1ebeed07b0b50aec0a
ryusuke920 <ryusuke920@icloud.com>
2022-08-11 01:40:44 +0900

    modified branch

--- Other/debug.py
+++ Other/debug.py
@@ -9,8 +9,8 @@ a = [1, 2, 3, 4, 5]
 word = "ryusuke"
 debug(a, word)
 
-# ABC199のAを解いてみる
-A, B, C = map(int,input().split())
+
+A, B, C = map(int, input().split())
 if A ** 2 + B ** 2 < C ** 2:
     print("Yes")
 else:
