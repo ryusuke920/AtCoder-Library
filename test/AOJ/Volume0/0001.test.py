aaca720a5cf787d23241cb24f8513fe5cbdb9922
ryusuke <66785066+ryusuke920@users.noreply.github.com>
2022-08-11 01:37:56 +0900

    Delete test directory

--- test/AOJ/Volume0/0001.test.py
+++
@@ -1,8 +0,0 @@
-# verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0001
-
-def main():
-    print(*sorted([int(input()) for _ in range(10)], reverse=True)[:3], sep='\n')
-
-
-if __name__ == "__main__":
-    main()
--- test/yosupo/Sample/A+B.test.py
+++
@@ -1,8 +0,0 @@
-# verification-helper: PROBLEM https://judge.yosupo.jp/problem/aplusb
-
-def main() -> None:
-    A, B = map(int, input().split())
-    print(A + B)
-
-if __name__ == "__main__":
-    main()
