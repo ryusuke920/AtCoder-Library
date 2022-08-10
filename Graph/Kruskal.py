a633d2d3013a829e5d78e94e2db6cd6c60db9742
ryusuke920 <ryusuke920@icloud.com>
2022-08-10 18:26:29 +0900

    add Kruskal problem

--- Graph/Kruskal.py
+++ Graph/Kruskal.py
@@ -1,43 +1,58 @@
-# クラスカル法
-# 出典：https://atcoder.jp/contests/typical-algorithm/tasks/typical_algorithm_f
+class Kruskal:
+    def __init__(self, n: int, g: list) -> None:
+        self.n = n
+        self.g = g
+        self.p = [-1] * n
 
-class UnionFind:
-  def __init__(self, n):
-    self.n = n
-    self.p = [-1] * n
 
-  def leader(self, a):
-    while self.p[a] >= 0:
-      a = self.p[a]
-    return a
+    def leader(self, a: int) -> int:
+        while self.p[a] >= 0:
+            a = self.p[a]
+        return a
 
-  def merge(self, a, b):
-    x = self.leader(a)
-    y = self.leader(b)
-    if x == y: return x
-    if self.p[x] > self.p[y]:
-      x, y = y, x
-    self.p[x] += self.p[y]
-    self.p[y] = x
-    return x
 
-  def same(self, a, b): return self.leader(a) == self.leader(b)
+    def merge(self, a: int, b: int) -> int:
+        x = self.leader(a)
+        y = self.leader(b)
 
-  def size(self, a): return -self.p[self.leader(a)]
+        if x == y:
+            return x
 
-n, m = map(int, input().split())
+        if self.p[x] > self.p[y]:
+            x, y = y, x
 
-uf = UnionFind(n)
+        self.p[x] += self.p[y]
+        self.p[y] = x
 
-# (a, b, cost)の無向グラフ
+        return x
 
-g = [list(map(int, input().split())) for _ in range(m)]
-g.sort(key=lambda x: x[2])
 
-ans = 0
-for i, j, cost in g:
-    if uf.same(i, j): continue
-    uf.merge(i, j)
-    ans += cost
+    def same(self, a: int, b: int) -> bool:
+        return self.leader(a) == self.leader(b)
 
-print(ans)
+
+    def size(self, a: int) -> int:
+        return -self.p[self.leader(a)]
+
+
+    def cost(self, cost: int) -> list:
+        tree = []
+        for i, j, cost in self.g:
+            if self.same(i, j):
+                continue
+            self.merge(i, j)
+            tree.append(cost)
+        
+        return tree
+
+
+def main() -> None:
+    n, m = map(int, input().split())
+    g = [list(map(int, input().split())) for _ in range(m)]
+
+    Kruskal = Kruskal(n, g)
+    print(sum(Kruskal.tree(g)))
+
+
+if __name__ == "__main__":
+    main()
--- test/yukicoder/2007.test.py
+++
@@ -1,6 +0,0 @@
-# verification-helper: PROBLEM https://yukicoder.me/problems/no/2007
-
-a, n = map(int, input().split())
-
-print(10 ** 7)
-print(pow(a, n, 10 ** 7))
