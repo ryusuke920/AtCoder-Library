94d199584f9f3cb76be930947d524fcc919f2316
ryusuke920 <ryusuke_920@icloud.com>
2023-04-24 10:12:14 +0900

    add ITP1_3_D.test.py

--- MathLibrary/Divisor.py
+++ MathLibrary/Divisor.py
@@ -1,6 +1,6 @@
-def divisors(n):
+def divisors(n: int) -> list:
     divisor = []
-    for i in range(1, int(n ** 0.5) + 1):
+    for i in range(1, int(n**0.5) + 1):
         if n % i == 0:
             divisor.append(i)
             if i != n // i:
@@ -8,8 +8,3 @@ def divisors(n):
 
     divisor.sort()
     return divisor
-
-x = divisors(10 ** 12)
-y = divisors(136)
-print(len(x))
-print(y)
