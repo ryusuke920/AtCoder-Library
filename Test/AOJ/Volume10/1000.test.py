a734a776b369b4f5395f94da512ecc460196a22c
ryusuke920 <ryusuke_920@icloud.com>
2023-06-28 16:02:03 +0900

    solve 1000.test.py

---
+++ Test/AOJ/Volume10/1000.test.py
@@ -0,0 +1,13 @@
+# verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1000&lang=en
+
+
+def main():
+    try:
+        a, b = map(int, input().split())
+        print(a + b)
+    except:
+        exit()
+
+
+if __name__ == "__main__":
+    main()
