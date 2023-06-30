d69c4de28b221ab4d4e8a8d7a3a149bb34799c13
ryusuke920 <ryusuke_920@icloud.com>
2023-06-30 15:49:40 +0900

    modified error

--- Test/AOJ/ITP1/ALDS1_10_C.test.py
+++ Test/AOJ/ITP1/ALDS1_10_C.test.py
@@ -1,14 +1,14 @@
 # verification-helper: PROBLEM https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_10_C&lang=ja
 # verification-helper: ERROR 1e-4
 
-from statistics import stdev
+from statistics import pstdev
 
 def main():
     while True:
         n = int(input())
         if n == 0:
             exit()
-        print(stdev(list(map(int, input().split()))))
+        print(pstdev(list(map(int, input().split()))))
 
 
 if __name__ == "__main__":
