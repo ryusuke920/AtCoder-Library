6807921a02ef4c166e211ce4a26e24a3dee34d05
ryusuke920 <ryusuke_920@icloud.com>
2023-12-02 20:24:38 +0900

    modified 2570.test.py

--- Test/yukicoder/2570.test.py
+++ Test/yukicoder/2570.test.py
@@ -5,18 +5,6 @@ sys.path.append("../../")
 
 from Math import Divisor
 
-def divisors(n: int) -> list:
-    divisor = []
-    for i in range(1, int(n**0.5) + 1):
-        if n % i == 0:
-            divisor.append(i)
-            if i != n // i:
-                divisor.append(n // i)
-
-    divisor.sort()
-    return divisor
-
-
 def main() -> None:
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
